2f469a0cdca7b8f20d39d8e7a1393395580caf8c
reganputra <ramadhanregan334@gmail.com>
2025-08-29 08:59:45 +0700

    Refactor dependency initialization in ServerHttp; ensure LogoutAPI and HealthCheckRepo are correctly instantiated

--- cmd/http.go
+++ cmd/http.go
@@ -21,6 +21,7 @@ func ServerHttp() {
 	userV1 := r.Group("/users/v1")
 	userV1.POST("/register", deps.RegisterAPI.Register)
 	userV1.POST("/login", deps.LoginAPI.Login)
+	userV1.DELETE("/logout", deps.LogoutAPI.Logout)
 
 	err := r.Run(":" + helpers.GetEnv("PORT", "8080"))
 	if err != nil {
@@ -31,52 +32,54 @@ func ServerHttp() {
 type Dependency struct {
 	// Repositories
 	UserRepo        repository.UserRepository
-	HealthCheckRepo *repository.HealthCheckRepo
+	HealthCheckRepo repository.HealthCheckRepo
 
 	// Services
 	LoginService    service.LoginService
 	RegisterService service.RegisterService
-	HealthCheckSvc  *service.HealthCheck
+	HealthCheckSvc  service.HealthCheck
+	LogoutService   service.LogoutService
 
 	// API Handlers
 	LoginAPI       api.LoginHandler
 	RegisterAPI    api.RegisterHandler
 	HealthCheckAPI api.HealthCheck
+	LogoutAPI      api.LogoutHandler
 }
 
 func InitializeDependencies() *Dependency {
 	deps := &Dependency{}
 
 	// Initialize Repositories
-	deps.HealthCheckRepo = repository.NewHealthCheckRepo()
-	deps.UserRepo = repository.UserRepository{
-		DB: helpers.DB,
-	}
+	deps.HealthCheckRepo = *repository.NewHealthCheckRepo()
+	deps.UserRepo = repository.UserRepository{DB: helpers.DB}
 
 	// Initialize Services
-	deps.HealthCheckSvc = &service.HealthCheck{
-		HealthCheckRepository: deps.HealthCheckRepo,
+	deps.HealthCheckSvc = service.HealthCheck{
+		HealthCheckRepository: &deps.HealthCheckRepo,
 	}
-
 	deps.RegisterService = service.RegisterService{
 		UserRepo: &deps.UserRepo,
 	}
-
 	deps.LoginService = service.LoginService{
+		UserRepo: &deps.UserRepo,
+	}
+	deps.LogoutService = service.LogoutService{
 		UserRepo: &deps.UserRepo,
 	}
 
 	// Initialize API Handlers
 	deps.HealthCheckAPI = api.HealthCheck{
-		HealthCheckService: deps.HealthCheckSvc,
+		HealthCheckService: &deps.HealthCheckSvc,
 	}
-
 	deps.RegisterAPI = api.RegisterHandler{
 		RegisterService: &deps.RegisterService,
 	}
-
 	deps.LoginAPI = api.LoginHandler{
 		LoginService: &deps.LoginService,
+	}
+	deps.LogoutAPI = api.LogoutHandler{
+		LogoutService: &deps.LogoutService,
 	}
 
 	return deps
